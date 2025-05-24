b8165c75593a192958ce248cb5820a81bf6bae23
mbroumsadja <mbroumsadjaa@gmail.com>
2025-05-24 04:57:54 +0100

    Refactor code structure for improved readability and maintainability

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
 import { dirname, join } from 'node:path';
 import { Server } from 'socket.io';
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
 import { availableParallelism } from 'node:os';
 import cluster from 'node:cluster';
 import { createAdapter, setupPrimary } from '@socket.io/cluster-adapter';
@@ -19,19 +17,6 @@ if (cluster.isPrimary) {
 
   setupPrimary();
 } else {
-  const db = await open({
-    filename: 'chat.db',
-    driver: sqlite3.Database
-  });
-
-  await db.exec(`
-    CREATE TABLE IF NOT EXISTS messages (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      client_offset TEXT UNIQUE,
-      content TEXT
-    );
-  `);
-
   const app = express();
   const server = createServer(app);
   const io = new Server(server, {
@@ -45,35 +30,11 @@ if (cluster.isPrimary) {
     res.sendFile(join(__dirname, 'index.html'));
   });
 
-  io.on('connection', async (socket) => {
-    socket.on('chat message', async (msg, clientOffset, callback) => {
-      let result;
-      // try {
-      //   result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', msg, clientOffset);
-      // } catch (e) {
-      //   if (e.errno === 19 /* SQLITE_CONSTRAINT */ ) {
-      //     callback();
-      //   } else {
-      //     // nothing to do, just let the client retry
-      //   }
-      //   return;
-      // }
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg, clientOffset, callback) => {
       io.emit('chat message', msg);
       callback();
     });
-
-    // if (!socket.recovered) {
-    //   try {
-    //     await db.each('SELECT id, content FROM messages WHERE id > ?',
-    //       [socket.handshake.auth.serverOffset || 0],
-    //       (_err, row) => {
-    //         socket.emit('chat message', row.content, row.id);
-    //       }
-    //     )
-    //   } catch (e) {
-    //     // something went wrong
-    //   }
-    // }
   });
 
   const port = process.env.PORT;
